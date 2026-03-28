a05f776c00174638a380ae2ab7707072b9eef784
satyamluci2407 <satyamshandilya2407@gmail.com>
2026-03-28 19:43:02 +0530

    added voice to bubu

--- engine/command.py
+++ engine/command.py
@@ -1,1 +1,13 @@
 import eel
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    print(voices)
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello, I am Bubu, your voice assistant. How can I assist you today?")
--- engine/features.py
+++ engine/features.py
@@ -6,3 +6,9 @@ from playsound import playsound
 def playAssistantSound():
     music_dir = "www\\assets\\audio\\bubu_slow.mp3"
     playsound(music_dir)
+    
+@eel.expose
+def playClickSound():
+    music_dir = "www\\assets\\audio\\clickSound.mp3"
+    playsound(music_dir)
+ 
